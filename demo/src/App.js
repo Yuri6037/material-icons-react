36fcd4faa8d1cfd8ef6f7305bb24d9699bb9fbb2
Kirill Shekhovtsov <ksedline@gmail.com>
2020-04-19 01:46:06 +0300

    Separate index.css from React Component to support SCSS Next Loader

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MaterialIcon, {colorPalette} from 'material-icons-react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "node_modules/material-icons-react/index.css";
+import "./App.css";
+import MaterialIcon, { colorPalette } from "material-icons-react";
 
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      inactive: false
-    }
+      inactive: false,
+    };
   }
 
   render() {
-
-    let loader = <div className="lds-ripple"><div></div><div></div></div>
+    let loader = (
+      <div className="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+    );
 
     return (
       <div className="App">
@@ -28,47 +32,75 @@ class App extends Component {
             <tbody>
               <tr>
                 <td>Default</td>
-                <td><MaterialIcon icon="dashboard" /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" />
+                </td>
               </tr>
               <tr>
                 <td>Default</td>
-                <td><MaterialIcon icon="alarm_on" /></td>
+                <td>
+                  <MaterialIcon icon="alarm_on" />
+                </td>
               </tr>
               <tr>
                 <td>Large</td>
-                <td><MaterialIcon icon="dashboard" size='large' /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" size="large" />
+                </td>
               </tr>
               <tr>
                 <td>Custom size(100)</td>
-                <td><MaterialIcon icon="dashboard" size={100} /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" size={100} />
+                </td>
               </tr>
               <tr>
                 <td>Inverted</td>
-                <td><MaterialIcon icon="dashboard" invert /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" invert />
+                </td>
               </tr>
               <tr>
                 <td>Inactive</td>
-                <td><MaterialIcon icon="dashboard" inactive /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" inactive />
+                </td>
               </tr>
               <tr>
                 <td>Color from pallete(amber._200)</td>
-                <td><MaterialIcon icon="dashboard" color={colorPalette.amber._200}/></td>
+                <td>
+                  <MaterialIcon
+                    icon="dashboard"
+                    color={colorPalette.amber._200}
+                  />
+                </td>
               </tr>
               <tr>
                 <td>Color from pallete(amber.A700)</td>
-                <td><MaterialIcon icon="dashboard" color={colorPalette.amber.A700}/></td>
+                <td>
+                  <MaterialIcon
+                    icon="dashboard"
+                    color={colorPalette.amber.A700}
+                  />
+                </td>
               </tr>
               <tr>
                 <td>Custom color(#7bb92f)</td>
-                <td><MaterialIcon icon="dashboard" color='#7bb92f' /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" color="#7bb92f" />
+                </td>
               </tr>
               <tr>
                 <td>Preloader</td>
-                <td><MaterialIcon icon="dashboard" preloader={loader} /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" preloader={loader} />
+                </td>
               </tr>
               <tr>
                 <td>Additional props</td>
-                <td><MaterialIcon icon="dashboard" test="test prop" /></td>
+                <td>
+                  <MaterialIcon icon="dashboard" test="test prop" />
+                </td>
               </tr>
             </tbody>
           </table>
