e23b9ad121c608448fe96974796cfe18d08a5898
Kirill Shekhovtsov <ksedline@gmail.com>
2020-04-19 15:13:55 +0300

    Update typing

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,11 @@ import React from "react";
 interface Props {
   icon: string;
   size?: number | string;
-  invert?: bool;
-  inactive?: bool;
+  invert?: boolean;
+  inactive?: boolean;
   color?: string;
 }
 
-declare function MaterialIcon(p: Props): React.ReactNode;
+declare function MaterialIcon(props: Props): React.ReactNode;
 
 export default MaterialIcon;
